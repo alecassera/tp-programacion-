c4d70aef5e571ff2e570b5fe5696fc111f36e632
Martina Bellsola <martinabellsola@MacBook-Air-de-Martina.local>
2020-06-13 13:57:58 -0300

    top 10 artistas generos (13/06/20) Martina

--- generos/generosDetalle.js
+++ generos/generosDetalle.js
@@ -23,4 +23,21 @@ window.addEventListener ('load', function(){
 
     })
 
+    // Top 10 artistas para generos detalle 
+
+    fetch(" https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/" + GenerosDetallePrueba + "/artists") 
+    .then(function(response) {
+      return response.json()
+    })
+    .then(
+      function(resultado) {  
+      console.log(resultado)
+      for (let index = 0; index < 10; index++) {
+        let nombreArtistas = resultado.data[index].name;
+        
+        let NuevoHtml = "<li >" + nombreArtistas + "</li> "
+        document.querySelector( " .ListadoArtistas " ). innerHTML += NuevoHtml
+
+      }
+      })
 })
